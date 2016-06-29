f1143a6db3635485c1d1ce902546b3f267a5d40b
red0509 <paa_pavlov@mail.ru>
2016-06-29 20:14:12 +0300

    Initial Commit

---
+++ MMIS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MMIS
+//
+//  Created by Anton Pavlov on 29.06.16.
+//  Copyright © 2016 Anton Pavlov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MMIS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MMIS
+//
+//  Created by Anton Pavlov on 29.06.16.
+//  Copyright © 2016 Anton Pavlov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
