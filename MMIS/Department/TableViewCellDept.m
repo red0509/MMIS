277c5000abcbbaab5ba71e6c19d67e47661f08e4
Anton <paapavlov@gmail.com>
2016-09-18 11:32:45 +0300

    1.4.1
    iOS 10

--- MMIS/Department/TableViewCellDept.m
+++ MMIS/Department/TableViewCellDept.m
@@ -11,12 +11,12 @@
 @implementation TableViewCellDept
 
 - (void)awakeFromNib {
-    // Initialization code
+    [super awakeFromNib];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
+    
     // Configure the view for the selected state
 }
 
--- MMIS/Group/Selection/Register/TableViewCellRegister.m
+++ MMIS/Group/Selection/Register/TableViewCellRegister.m
@@ -11,6 +11,7 @@
 @implementation TableViewCellRegister
 
 - (void)awakeFromNib {
+     [super awakeFromNib];
     // Initialization code
 }
 
